727a405a0a020a24d2bec09ac287e79badc31dfd
Saksham Virmani <76741579+sakshamvirmani@users.noreply.github.com>
2025-06-25 12:46:22 +0530

    Create promptHelpers.js
    Placeholder for prompt formatting, embedding prep

---
+++ vertex-utils/promptHelpers.js
@@ -0,0 +1,17 @@
+// vertex-utils/promptHelpers.js
+
+function buildPrompt(question, contextChunk) {
+  return `
+You are a helpful assistant. Use the context below to answer the question.
+
+Context:
+${contextChunk}
+
+Question:
+${question}
+
+Answer:
+`;
+}
+
+module.exports = { buildPrompt };
